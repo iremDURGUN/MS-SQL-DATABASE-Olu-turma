e31da0600a170b51c588b61eff0d35d8ea8891f0
İrem Durgun <116745736+iremDURGUN@users.noreply.github.com>
2024-02-07 10:34:59 +0300

    SQL Query Examples.sql

--- SQL Query Examples.sql
+++ SQL Query Examples.sql
@@ -1,34 +1,36 @@
+/*use ile Ã§alÄ±ÅŸacaÄŸÄ±mÄ±z database 'i belirtiriz.*/
+use LibProject 
 
-/*Bu sorgu sayesinde kitap adı K harfiyle başlayan kitapların hangi yazara ait olduğunu sorgularız*/ 
+/*Bu sorgu sayesinde kitap adÄ± K harfiyle baÅŸlayan kitaplarÄ±n hangi yazara ait olduÄŸunu sorgularÄ±z*/ 
 	SELECT w.writerName,a.bookName FROM writers AS w
         INNER JOIN allbooks AS a on w.ID=a.writerID 
         WHERE a.bookName LIKE 'K%' 
         
 
 
-	/* Bu sorgu sayesinde 2018 tarihinden sonra basılan kitapların basım tarihlerini ve stok tipilerini görürüz.*/
+	/* Bu sorgu sayesinde 2018 tarihinden sonra basÄ±lan kitaplarÄ±n basÄ±m tarihlerini ve stok tipilerini gÃ¶rÃ¼rÃ¼z.*/
 	SELECT a.addedDate,s.stockTypeName FROM allbooks AS a
 	INNER JOIN stockType AS s on a.stockTypeID=s.ID
 	WHERE a.addedDate > '2018-12-30'
 
 
-	/*Bu sorgu sayesinde 2018 yıllından önce kaç adet bağışlanan, kaç adet satın alınan kitap olduğunu gördüğümüz sorgudur.*/
+	/*Bu sorgu sayesinde 2018 yÄ±llÄ±ndan Ã¶nce kaÃ§ adet baÄŸÄ±ÅŸlanan, kaÃ§ adet satÄ±n alÄ±nan kitap olduÄŸunu gÃ¶rdÃ¼ÄŸÃ¼mÃ¼z sorgudur.*/
 	SELECT COUNT(s.stockTypeName) AS AdetSayisi ,s.stockTypeName  FROM allbooks AS a
 	JOIN stockType AS s on a.stockTypeID=s.ID
 	WHERE a.addedDate < '2018-12-30' GROUP BY s.stockTypeName
 
 
-	/*Bu sorgu sayesinde yazarların isimlerin de kısaltma kullanan toplam kaç kişi vardır belirleriz.*/
+	/*Bu sorgu sayesinde yazarlarÄ±n isimlerin de kÄ±saltma kullanan toplam kaÃ§ kiÅŸi vardÄ±r belirleriz.*/
 	SELECT COUNT(writerName) AS Adet  FROM writers
 	WHERE writerName LIKE '%.%' 
 
-	/*Bu sorgu sayesinde yazarların isimlerin de kısaltma kullananların isimlerini belirleriz.*/
+	/*Bu sorgu sayesinde yazarlarÄ±n isimlerin de kÄ±saltma kullananlarÄ±n isimlerini belirleriz.*/
     SELECT writerName FROM writers
 	WHERE writerName LIKE '%.%' GROUP BY writerName
 
-	/*Stored Procedure yardımıyla bir fonksiyon oluşturduk.
-	allBooks tablosundaki yazarların tabloda kaç adet kitapları var 
-	ve tabloda bulunan kitap türlerinden kaçar adet var hesaplanır.
+	/*Stored Procedure yardÄ±mÄ±yla bir fonksiyon oluÅŸturduk.
+	allBooks tablosundaki yazarlarÄ±n tabloda kaÃ§ adet kitaplarÄ± var 
+	ve tabloda bulunan kitap tÃ¼rlerinden kaÃ§ar adet var hesaplanÄ±r.
 	 */
 
 	use LibProject
@@ -45,15 +47,15 @@
 	execute Hesapla
 
 
-	/*Stored Procedure yardımıyla bir fonksiyon oluşturduk. 
-	allbooks tablosunda her türde yazarların kaçar adet kitabının olduğunu sorgululamamızı sağlayan sorgudur.*/
+	/*Stored Procedure yardÄ±mÄ±yla bir fonksiyon oluÅŸturduk. 
+	allbooks tablosunda her tÃ¼rde yazarlarÄ±n kaÃ§ar adet kitabÄ±nÄ±n olduÄŸunu sorgululamamÄ±zÄ± saÄŸlayan sorgudur.*/
 	
 	go
 	ALTER PROCEDURE yazarTurIliskisi
 	AS
 	BEGIN
 
-		SELECT COUNT(a.writerName) AS 'Yazar Adet Sayısı',a.writerName, a.bookTypeName
+		SELECT COUNT(a.writerName) AS 'Yazar Adet SayÄ±sÄ±',a.writerName, a.bookTypeName
 		FROM allbooks AS a
 		GROUP BY a.writerName ,a.bookTypeName
 		
@@ -61,7 +63,7 @@
 
 	execute yazarTurIliskisi
 
-	/* publisher tablosunda publisherName kolonunda Yayınları yazan yerler Yayınevi olarak değiştirmemizi sağlayan sorgudur.*/
+	/* publisher tablosunda publisherName kolonunda YayÄ±nlarÄ± yazan yerler YayÄ±nevi olarak deÄŸiÅŸtirmemizi saÄŸlayan sorgudur.*/
 	
 	go
 	ALTER PROCEDURE degistir
@@ -70,7 +72,7 @@
 
 	  select *,
 	  CASE 
-	   when publisherName like '%Yayınları%' then 'Yayinevi' 
+	   when publisherName like '%YayÄ±nlarÄ±%' then 'Yayinevi' 
 	   else
 		publisherName
 	    end
@@ -82,141 +84,141 @@
 
 	
 
-	/* allBooks tablosuna en eski basılan 5 kitabı en eski tarihten en yeni basılana doğru sıralı olarak sorgular. Bu işlemi ASC kullanarakta alabiliriz.*/
+	/* allBooks tablosuna en eski basÄ±lan 5 kitabÄ± en eski tarihten en yeni basÄ±lana doÄŸru sÄ±ralÄ± olarak sorgular. Bu iÅŸlemi ASC kullanarakta alabiliriz.*/
 	Select  top 5 a.addedDate, a.bookName FROM allbooks AS a ORDER BY a.addedDate
 
-   /* allBooks tablosunda en eski basım olan 5 kitabı en eski tarihten en yeni basılana doğru sıralı olarak sorgulamamızı sağlar.*/
+   /* allBooks tablosunda en eski basÄ±m olan 5 kitabÄ± en eski tarihten en yeni basÄ±lana doÄŸru sÄ±ralÄ± olarak sorgulamamÄ±zÄ± saÄŸlar.*/
 	Select  top 5 a.addedDate, a.bookName FROM allbooks AS a ORDER BY a.addedDate ASC
 
-   /*Yukardaki iki sorgunun aynı sonucu vermesinin nedeni ORDER BY varsayılan olarak ASC sıralamasında çalışır.*/
+   /*Yukardaki iki sorgunun aynÄ± sonucu vermesinin nedeni ORDER BY varsayÄ±lan olarak ASC sÄ±ralamasÄ±nda Ã§alÄ±ÅŸÄ±r.*/
 
-	/* allBooks tablosuna en son basılan 5 kitabı en yakın tarihten en eskisine doğru sıralı olarak sorgular.*/
+	/* allBooks tablosuna en son basÄ±lan 5 kitabÄ± en yakÄ±n tarihten en eskisine doÄŸru sÄ±ralÄ± olarak sorgular.*/
 	Select  top 5 a.addedDate, a.bookName FROM allbooks AS a ORDER BY a.addedDate DESC
 
 
 	
 
 
-	/*bookTypeName tablosunda ID numarası tek ve çift olanları aynı tabloda önce tek olanlar sonra çift olanlar şeklinde sıralayan sorgudur.*/
+	/*bookTypeName tablosunda ID numarasÄ± tek ve Ã§ift olanlarÄ± aynÄ± tabloda Ã¶nce tek olanlar sonra Ã§ift olanlar ÅŸeklinde sÄ±ralayan sorgudur.*/
 	  select * from bookTypeName where ID % 2 = 1
     union all
     select *  from bookTypeName where ID % 2 = 0
 
 
-	/*Publisher ve allbooks tablosunda ID değerleri çift olan publisherName ve bookName'leri sorgulayaran ve ID'leriyle çıktı veren sorgudur.*/
+	/*Publisher ve allbooks tablosunda ID deÄŸerleri Ã§ift olan publisherName ve bookName'leri sorgulayaran ve ID'leriyle Ã§Ä±ktÄ± veren sorgudur.*/
 	  SELECT  p.ID, p.publisherName,a.ID,a.bookName FROM allbooks AS a
 	  INNER JOIN publisher AS p ON a.publisherID = p.ID
 	  WHERE a.ID % 2 = 0 
 
 
-	/*allbooks tablosunda toplam  birden çok kitap'ı olan kaç yazar olduğunu hesaplayan sorgudur.*/
-	SELECT COUNT(writerName) - COUNT(DISTINCT(writerID)) AS 'Listede Birden Çok Kitabı Olan Yazar Sayisi'  
+	/*allbooks tablosunda toplam  birden Ã§ok kitap'Ä± olan kaÃ§ yazar olduÄŸunu hesaplayan sorgudur.*/
+	SELECT COUNT(writerName) - COUNT(DISTINCT(writerID)) AS 'Listede Birden Ã‡ok KitabÄ± Olan Yazar Sayisi'  
 	FROM allbooks
 
-	/*allbooks tablosunda alfabetik olarak kitap adı en kısa olan kitabı sorgular */
+	/*allbooks tablosunda alfabetik olarak kitap adÄ± en kÄ±sa olan kitabÄ± sorgular */
 	SELECT TOP 1 bookName,LEN(bookName) AS Uzunluk FROM allbooks 
 	WHERE LEN(bookName) = (SELECT TOP 1 MIN(LEN(bookName)) AS Uzunluk FROM allbooks) 
         GROUP BY bookName ORDER BY bookName ASC
 
 
-	/*allbooks tablosunda alfabetik olarak kitap adı en uzun olan kitabı sorgular */
+	/*allbooks tablosunda alfabetik olarak kitap adÄ± en uzun olan kitabÄ± sorgular */
     SELECT TOP 1 bookName,LEN(bookName) AS Uzunluk FROM allbooks 
   	WHERE LEN(bookName)=(SELECT TOP 1 MAX(LEN(bookName)) AS Uzunluk FROM allbooks) 
         GROUP BY bookName ORDER BY bookName ASC
 
 
-	/*allBooks tablosunda isbnID 'de 4. karakteri 9 olmayan satırları döndüren sorgudur.*/
+	/*allBooks tablosunda isbnID 'de 4. karakteri 9 olmayan satÄ±rlarÄ± dÃ¶ndÃ¼ren sorgudur.*/
 	SELECT* FROM allbooks WHERE isbnID NOT LIKE '___[9]%'
 
 
-    /* allbooks tablosunda bookName sütunun'da tekrar etmeyen verilerde a,e,i,o,u harflerinin kitap isimlerinin hem başında hem de sonunda olmayan kitap isimlerini sorgular. */
+    /* allbooks tablosunda bookName sÃ¼tunun'da tekrar etmeyen verilerde a,e,i,o,u harflerinin kitap isimlerinin hem baÅŸÄ±nda hem de sonunda olmayan kitap isimlerini sorgular. */
 	SELECT DISTINCT(bookName) FROM allbooks WHERE bookName NOT LIKE '[aeiom]%' AND bookName NOT LIKE '%[aeiom]'
 
-   /*allbooks tablosunda her katagoride kaçar adet kitap olduğunu küçükten büyüğe sıralar. Bunu yaparken katagori adlarını alfabetik olarakta sıralar. 
-	  Eğer alfabetik sıralamada aynı harfle başlayan katagoriler varsa bunları kendi içinde adet sayısına göre küçükten büyüğe sıralar. */
-	SELECT CONCAT(bookTypeName,' ','katagorisinde',' ',COUNT(*), ' ','adet kitap vardır.') AS Katagori_Adetleri
+   /*allbooks tablosunda her katagoride kaÃ§ar adet kitap olduÄŸunu kÃ¼Ã§Ã¼kten bÃ¼yÃ¼ÄŸe sÄ±ralar. Bunu yaparken katagori adlarÄ±nÄ± alfabetik olarakta sÄ±ralar. 
+	  EÄŸer alfabetik sÄ±ralamada aynÄ± harfle baÅŸlayan katagoriler varsa bunlarÄ± kendi iÃ§inde adet sayÄ±sÄ±na gÃ¶re kÃ¼Ã§Ã¼kten bÃ¼yÃ¼ÄŸe sÄ±ralar. */
+	SELECT CONCAT(bookTypeName,' ','katagorisinde',' ',COUNT(*), ' ','adet kitap vardÄ±r.') AS Katagori_Adetleri
         FROM allbooks
         GROUP BY bookTypeName
         ORDER BY  Katagori_Adetleri , bookTypeName ASC;
 
 	/*Notlar:  
 
-	INNER JOIN : İki ya da daha fazla tabloda ortak olan iki alandaki değerleri kontrol ederek tabloları birleştirir.
-	  İki ya da daha fazla tabloda ortak olan iki alandaki değerleri kontrol ederek tabloları birleştirir. 
-	  INNER JOIN, SQL sunucusunda varsayılan olan JOINdir. Yani INNER JOIN yerine sadece JOIN yazmak da yeterlidir. 
-	  WHERE ifadesi kullanarak oluşturmak istenen sonuç tablosu filitrelenerek daha da özelleştirilebilir.
+	INNER JOIN : Ä°ki ya da daha fazla tabloda ortak olan iki alandaki deÄŸerleri kontrol ederek tablolarÄ± birleÅŸtirir.
+	  Ä°ki ya da daha fazla tabloda ortak olan iki alandaki deÄŸerleri kontrol ederek tablolarÄ± birleÅŸtirir. 
+	  INNER JOIN, SQL sunucusunda varsayÄ±lan olan JOINdir. Yani INNER JOIN yerine sadece JOIN yazmak da yeterlidir. 
+	  WHERE ifadesi kullanarak oluÅŸturmak istenen sonuÃ§ tablosu filitrelenerek daha da Ã¶zelleÅŸtirilebilir.
 
-	COUNT : Kolonda veya sütunda hesaplanmak istenen sayıyı hesaplar.
+	COUNT : Kolonda veya sÃ¼tunda hesaplanmak istenen sayÄ±yÄ± hesaplar.
 
-	LIKE : Bir sütundaki belirli bir veriyi aramak için bir WHERE koşuluyla kullanılır. 
-	  Aranmak istenen karakter % imleçi yardımıyla tasvir edilerek arama işlemi yapılır. 
-	  Örneğin '%A' ifadesi A karakteri ile biten ifadeleri aramak için kullanılır.
-	   'A%' ifadesi A karakteri ile başlayan ifadeleri aramak için kullanılır.
-	   '%A%' ifadesi sağında ve solunda başka karakterlerinde olduğu ifadeleri arar. Örneğin: KAR kelimesini aramak için kullanılmalıdır.
+	LIKE : Bir sÃ¼tundaki belirli bir veriyi aramak iÃ§in bir WHERE koÅŸuluyla kullanÄ±lÄ±r. 
+	  Aranmak istenen karakter % imleÃ§i yardÄ±mÄ±yla tasvir edilerek arama iÅŸlemi yapÄ±lÄ±r. 
+	  Ã–rneÄŸin '%A' ifadesi A karakteri ile biten ifadeleri aramak iÃ§in kullanÄ±lÄ±r.
+	   'A%' ifadesi A karakteri ile baÅŸlayan ifadeleri aramak iÃ§in kullanÄ±lÄ±r.
+	   '%A%' ifadesi saÄŸÄ±nda ve solunda baÅŸka karakterlerinde olduÄŸu ifadeleri arar. Ã–rneÄŸin: KAR kelimesini aramak iÃ§in kullanÄ±lmalÄ±dÄ±r.
 	   '_a%' ifadesi ikinci harfi a olan ifadeleri bulur.
-	   'a%o' ifadesi a ile başlayan o ile biten ifadeleri bulur.
-	   'a__%' ifadesi a ile başlayan sonrasında sağında en azından 3 karakter olan ifadeleri bulur.
+	   'a%o' ifadesi a ile baÅŸlayan o ile biten ifadeleri bulur.
+	   'a__%' ifadesi a ile baÅŸlayan sonrasÄ±nda saÄŸÄ±nda en azÄ±ndan 3 karakter olan ifadeleri bulur.
 
-  NOT LIKE: Not like ifadesi LIKE ifadesinin tersi olarak çalışır. Sütunda arama yaparken aramadan hariç tutmak istediğimiz ifadeleri belirterek kullanırız.
+  NOT LIKE: Not like ifadesi LIKE ifadesinin tersi olarak Ã§alÄ±ÅŸÄ±r. SÃ¼tunda arama yaparken aramadan hariÃ§ tutmak istediÄŸimiz ifadeleri belirterek kullanÄ±rÄ±z.
 
-	AS Operotörü : Sorgulama yaparken uzun ve karışık sütun isimlerini kolay kullanabilmek için kullanılan operotördür.
-	  AS sayesinde sorgulama yaparken birden fazla tablo ile çalışırken tablolara takma isim vererek kullanım kolaylığı sağlanır.
-	  Örnek olarak yukarda ınner joın ile yaptığımız sorgularda her tabloya isim vererek çalışılmıştır.
-	  Ya da örneğin count ile bir işlem yapıyorsak çalıştığımız kolona anlaşırlık açısından isim vermek için kullanılır.
+	AS OperotÃ¶rÃ¼ : Sorgulama yaparken uzun ve karÄ±ÅŸÄ±k sÃ¼tun isimlerini kolay kullanabilmek iÃ§in kullanÄ±lan operotÃ¶rdÃ¼r.
+	  AS sayesinde sorgulama yaparken birden fazla tablo ile Ã§alÄ±ÅŸÄ±rken tablolara takma isim vererek kullanÄ±m kolaylÄ±ÄŸÄ± saÄŸlanÄ±r.
+	  Ã–rnek olarak yukarda Ä±nner joÄ±n ile yaptÄ±ÄŸÄ±mÄ±z sorgularda her tabloya isim vererek Ã§alÄ±ÅŸÄ±lmÄ±ÅŸtÄ±r.
+	  Ya da Ã¶rneÄŸin count ile bir iÅŸlem yapÄ±yorsak Ã§alÄ±ÅŸtÄ±ÄŸÄ±mÄ±z kolona anlaÅŸÄ±rlÄ±k aÃ§Ä±sÄ±ndan isim vermek iÃ§in kullanÄ±lÄ±r.
 
-   WHERE : Bir koşul belirterek filtreleme yapmak için kullanılır. Where cümleciği koşul belirtmek için kullanılan bir komuttur.
+   WHERE : Bir koÅŸul belirterek filtreleme yapmak iÃ§in kullanÄ±lÄ±r. Where cÃ¼mleciÄŸi koÅŸul belirtmek iÃ§in kullanÄ±lan bir komuttur.
 
-   GROUP BY : Tabloda ki aynı değerlere sahip verileri gruplamamızı sağlar. Verileri bir veya daha fazla sütunla gruplamak için 
-      toplama işlevleri kullanır. Örneğin COUNT, MAX, MIN, SUM, AVG gibi işlemlerle kullanılır.
+   GROUP BY : Tabloda ki aynÄ± deÄŸerlere sahip verileri gruplamamÄ±zÄ± saÄŸlar. Verileri bir veya daha fazla sÃ¼tunla gruplamak iÃ§in 
+      toplama iÅŸlevleri kullanÄ±r. Ã–rneÄŸin COUNT, MAX, MIN, SUM, AVG gibi iÅŸlemlerle kullanÄ±lÄ±r.
 
-   ORDER BY : verileri artan ya da azalan düzende sıralamak için kullanılır. Kayıtları varsayılan olarak artan sırada sıralar.
-      Kayıtları azalan sırada sıralamak için DESC ifadesini kullanır.
+   ORDER BY : verileri artan ya da azalan dÃ¼zende sÄ±ralamak iÃ§in kullanÄ±lÄ±r. KayÄ±tlarÄ± varsayÄ±lan olarak artan sÄ±rada sÄ±ralar.
+      KayÄ±tlarÄ± azalan sÄ±rada sÄ±ralamak iÃ§in DESC ifadesini kullanÄ±r.
 
-   ASC : Listeyi artan sıraya göre sıralayacağımız belirtmek için kullanırız.
+   ASC : Listeyi artan sÄ±raya gÃ¶re sÄ±ralayacaÄŸÄ±mÄ±z belirtmek iÃ§in kullanÄ±rÄ±z.
 
-   DESC : Listeyi azalan sıraya göre sıralayacağımız belirtmek için kullanırız.
+   DESC : Listeyi azalan sÄ±raya gÃ¶re sÄ±ralayacaÄŸÄ±mÄ±z belirtmek iÃ§in kullanÄ±rÄ±z.
 
-   SELF JOIN : Bir tablonun kendisiyle JOIN işlemi yapılmasına Self Join denir. Hiyerarşik verileri sorgulamak 
-     veya aynı tablodaki satırları karşılaştırmak için kullanışlıdır.
+   SELF JOIN : Bir tablonun kendisiyle JOIN iÅŸlemi yapÄ±lmasÄ±na Self Join denir. HiyerarÅŸik verileri sorgulamak 
+     veya aynÄ± tablodaki satÄ±rlarÄ± karÅŸÄ±laÅŸtÄ±rmak iÃ§in kullanÄ±ÅŸlÄ±dÄ±r.
 
-   UNION ALL : İki tablodan birden kayıt çekmek için kullanılır. Ancak çekilecek kolonların aynı veri tipinden 
-     olmaları gerekir. Ayrıca, çekilen veriler birbirlerinden farklı (distinct) olmalıdır.
-	   Aynı olan verilerden yalnızca birisini seçer. 
+   UNION ALL : Ä°ki tablodan birden kayÄ±t Ã§ekmek iÃ§in kullanÄ±lÄ±r. Ancak Ã§ekilecek kolonlarÄ±n aynÄ± veri tipinden 
+     olmalarÄ± gerekir. AyrÄ±ca, Ã§ekilen veriler birbirlerinden farklÄ± (distinct) olmalÄ±dÄ±r.
+	   AynÄ± olan verilerden yalnÄ±zca birisini seÃ§er. 
 
-   TOP : Sorgulanacak tabloda sıralı olarak istediğiniz satır sayısına göre çıktı döndürmeyi sağlar.
+   TOP : Sorgulanacak tabloda sÄ±ralÄ± olarak istediÄŸiniz satÄ±r sayÄ±sÄ±na gÃ¶re Ã§Ä±ktÄ± dÃ¶ndÃ¼rmeyi saÄŸlar.
 
-   DISTINCT : Veri tekrarını engellemek için kullanılır. Sorgulama yaparken aynı kolonda bulunan verilerde 
-     veri tekrarının önüne geçerek veriyi bir kez döndürür.
+   DISTINCT : Veri tekrarÄ±nÄ± engellemek iÃ§in kullanÄ±lÄ±r. Sorgulama yaparken aynÄ± kolonda bulunan verilerde 
+     veri tekrarÄ±nÄ±n Ã¶nÃ¼ne geÃ§erek veriyi bir kez dÃ¶ndÃ¼rÃ¼r.
 
-   LEN : String bir verinin karakter sayısını almamızı sağlar.
+   LEN : String bir verinin karakter sayÄ±sÄ±nÄ± almamÄ±zÄ± saÄŸlar.
 
-   CRUD : CREATE ,READ, UPDATE ,DELETE işlemleri'dir.
+   CRUD : CREATE ,READ, UPDATE ,DELETE iÅŸlemleri'dir.
 
-   Stored Procedure : Veritabanında CRUD gibi işlemlerde, her seferinde kodu tekrar yazmamız ve derlememiz gerekmektedir. 
-     Bu yüzden hem zaman hem de derleme açısından perormans kaybı oluşmaktadır. Bu gibi durumlarda Store Procedure, 
-     ile bu sıkıntıların önüne geçeriz. Böylece her seferinde aynı işlemleri yapma gereksinimi duymadan zamandan tasarruf etmiş oluruz.
-	 Kullanımı : CREATE PROCEDURE procedure_name 
+   Stored Procedure : VeritabanÄ±nda CRUD gibi iÅŸlemlerde, her seferinde kodu tekrar yazmamÄ±z ve derlememiz gerekmektedir. 
+     Bu yÃ¼zden hem zaman hem de derleme aÃ§Ä±sÄ±ndan perormans kaybÄ± oluÅŸmaktadÄ±r. Bu gibi durumlarda Store Procedure, 
+     ile bu sÄ±kÄ±ntÄ±larÄ±n Ã¶nÃ¼ne geÃ§eriz. BÃ¶ylece her seferinde aynÄ± iÅŸlemleri yapma gereksinimi duymadan zamandan tasarruf etmiÅŸ oluruz.
+	 KullanÄ±mÄ± : CREATE PROCEDURE procedure_name 
                  AS 
                  BEGIN 
                  SORGU
                  END  
 
-      Procedure bir kez CREATE yardımıyla oluşturulduktan sonra üstünde değişiklik yapılacak ise ALTER yardımıyla
-	  bu değişiklikler yapılır.
+      Procedure bir kez CREATE yardÄ±mÄ±yla oluÅŸturulduktan sonra Ã¼stÃ¼nde deÄŸiÅŸiklik yapÄ±lacak ise ALTER yardÄ±mÄ±yla
+	  bu deÄŸiÅŸiklikler yapÄ±lÄ±r.
 	             ALTER PROCEDURE procedure_name 
                  AS 
                  BEGIN 
                  SORGU
                  END  
 
-   EXECUTE : Ececute yardımıyla oluşturduğumuz Procedure' u çalıştırırız.
+   EXECUTE : Ececute yardÄ±mÄ±yla oluÅŸturduÄŸumuz Procedure' u Ã§alÄ±ÅŸtÄ±rÄ±rÄ±z.
 
-   MOD (%) : % işareti matematiksel ifade olarak bir sayının modunu almamıza yardımcı olur.
+   MOD (%) : % iÅŸareti matematiksel ifade olarak bir sayÄ±nÄ±n modunu almamÄ±za yardÄ±mcÄ± olur.
 
-   AND-OR  : Where koşulunu kullanırken birden fazla koşul belirtmek için AND,OR yapıları kullanılır. AND kullanıldığında belirtilen koşulların tamamının sağlanması beklenir
-         OR kullanıldığında koşullardan birinin sağlanması yeterlidir. Mantık ifadeleri mantığıyla işlem yapar.
+   AND-OR  : Where koÅŸulunu kullanÄ±rken birden fazla koÅŸul belirtmek iÃ§in AND,OR yapÄ±larÄ± kullanÄ±lÄ±r. AND kullanÄ±ldÄ±ÄŸÄ±nda belirtilen koÅŸullarÄ±n tamamÄ±nÄ±n saÄŸlanmasÄ± beklenir
+         OR kullanÄ±ldÄ±ÄŸÄ±nda koÅŸullardan birinin saÄŸlanmasÄ± yeterlidir. MantÄ±k ifadeleri mantÄ±ÄŸÄ±yla iÅŸlem yapar.
 
-   CONCAT : Bir veya daha fazla dizeyi-kolonu birleştirerek yeni bir dize oluşturmak için kullanılır.
+   CONCAT : Bir veya daha fazla dizeyi-kolonu birleÅŸtirerek yeni bir dize oluÅŸturmak iÃ§in kullanÄ±lÄ±r.
 
 			  */
 	
